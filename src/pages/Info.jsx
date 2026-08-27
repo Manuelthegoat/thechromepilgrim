2c43e907f128f4035f78018a0f744586e644e8a4
manuelthegoat <manuelcreation247@gmail.com>
2026-08-27 14:31:37 +0100

    gg

--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -1,5 +1,5 @@
-import Eyebrow from '../components/shared/Eyebrow';
-import './Info.css';
+import Eyebrow from "../components/shared/Eyebrow";
+import "./Info.css";
 
 function Info() {
   return (
@@ -7,14 +7,19 @@ function Info() {
       <div className="info__block">
         <Eyebrow>Refund policy</Eyebrow>
         <p className="info__text">
-          All sales are final, we do not offer refunds unless the item is unavailable or if the item is lost in transit.
+          All sales are final, we do not offer refunds unless the item is
+          unavailable or if the item is lost in transit.
         </p>
       </div>
 
       <div className="info__block">
         <Eyebrow>Shipping</Eyebrow>
         <p className="info__text">
-          All pre-orders are processed within 5-10 business days before they are sent out for delivery. Please confirm the delivery information for each item by reading its description. To ensure smooth communication, please provide a valid email and phone number when placing your order. Note that import duties may apply for customers in certain regions. For more info, refer to our shipping policy.
+          All pre-orders are processed within 5-10 business days before they are
+          sent out for delivery. Please confirm the delivery information for
+          each item by reading its description. To ensure smooth communication,
+          please provide a valid email and phone number when placing your order.
+          Note that import duties may apply for customers in certain regions.
         </p>
       </div>
     </section>

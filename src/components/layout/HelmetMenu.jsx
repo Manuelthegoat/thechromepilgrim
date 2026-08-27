9260d7027f671a5883403d89b7be20eb2db03ffb
manuelthegoat <manuelcreation247@gmail.com>
2026-08-27 17:37:56 +0100

    gg

--- src/components/layout/HelmetMenu.jsx
+++ src/components/layout/HelmetMenu.jsx
@@ -16,9 +16,9 @@ function HelmetMenu({ onClose }) {
         <i className="ti ti-x" aria-hidden="true" />
       </button>
       {MENU_ITEMS.map((item) => (
-        <Link key={item.to} to={item.to} className="helmet-menu__item" onClick={onClose}>
+        <a key={item.to} href={item.to} className="helmet-menu__item" onClick={onClose}>
           {item.label}
-        </Link>
+        </a>
       ))}
     </div>
   );
